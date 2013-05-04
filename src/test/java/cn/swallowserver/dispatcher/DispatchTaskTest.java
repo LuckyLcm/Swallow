ed65e8c8caf7ca6e8c01a9a1afcb6dc1090af009
LuckyLcm <642788304@qq.com>
2013-05-04 23:41:18 +0800

    message

---
+++ src/test/java/cn/swallowserver/dispatcher/DispatchTaskTest.java
@@ -0,0 +1,15 @@
+package cn.swallowserver.dispatcher;
+
+
+import org.junit.Test;
+
+/**
+ * @author ICMLucky
+ */
+public class DispatchTaskTest {
+
+    @Test
+    public void testRun() {
+
+    }
+}
