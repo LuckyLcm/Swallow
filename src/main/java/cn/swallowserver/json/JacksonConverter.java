9e76b651f427449134a6cc2c19c641aa6ce19191
LuckyLcm <642788304@qq.com>
2013-06-04 20:46:41 +0800

    jacksonConverter.java

--- src/main/java/cn/swallowserver/json/JacksonConverter.java
+++ src/main/java/cn/swallowserver/json/JacksonConverter.java
@@ -1,30 +1,23 @@
 package cn.swallowserver.json;
 
+import org.codehaus.jackson.map.ObjectMapper;
+
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * @author ICMLucky
  */
 public class JacksonConverter implements Converter {
+
     @Override
     public Map<String, Object> convert (String jsonMsg) {
-         jsonMsg = "{\"str\":\"string\",\"num\":\"123\"}" ;
-        try{
-            List<LinkedHashMap<String, Object>> list = objectMapper.readValue(jsonMsg, List.class);
-            for (int i= 0;i < list.size ();i++) {
-                Map<String, Object> map = list.get(i);
-               if( assertEquals ("string", map.get ("str")) && assertEquals (123, map.get ("num"))){
-                    System.out.println ("equal!!");
-                   return map;
-               }
-            }
-        } catch (IOException e){
-                  e.printStackTrace();
-        }
 
+        try {
+            return new ObjectMapper ().readValue (jsonMsg, Map.class);
+        } catch (IOException e) {
+            throw new RuntimeException (e);
+        }
 
 
     }
